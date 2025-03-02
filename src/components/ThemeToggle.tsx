fa0ec0ca930b82f22aec4f6ce10bb069e3ac35db
Brendan Lindsey <brendan_lindsey@outlook.com>
2025-03-02 11:23:49 -0800

    Fixed svg color for theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -42,14 +42,14 @@ export default function ThemeToggle() {
         >
           <path
             d="M12 17C14.7614 17 17 14.7614 17 12C17 9.23858 14.7614 7 12 7C9.23858 7 7 9.23858 7 12C7 14.7614 9.23858 17 12 17Z"
-            stroke="currentColor"
+            stroke="#797979"
             strokeWidth="2.5"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
           <path
             d="M12 1V3M12 21V23M4.22 4.22L5.64 5.64M18.36 18.36L19.78 19.78M1 12H3M21 12H23M4.22 19.78L5.64 18.36M18.36 5.64L19.78 4.22"
-            stroke="currentColor"
+            stroke="#797979"
             strokeWidth="2.5"
             strokeLinecap="round"
             strokeLinejoin="round"
